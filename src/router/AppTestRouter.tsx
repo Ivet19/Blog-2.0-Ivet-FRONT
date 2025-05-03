ec78dbad60d802c17c99405c8f871384c883b1be
Ivet Lopez <ivet19@live.com>
2025-05-03 14:54:43 +0200

    Create AppTestRouter component

---
+++ src/router/AppTestRouter.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router";
+import PostsPage from "../post/pages/PostsPage/PostsPage";
+import AddPostPage from "../post/pages/AddPostPage/AddPostPage";
+import PostDetailPage from "../post/pages/PostDetailPage/PostDetailPage";
+
+const AppTestRouter: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/posts" element={<PostsPage />} />
+      <Route path="/add-post" element={<AddPostPage />} />
+      <Route path="/post/:id" element={<PostDetailPage />} />
+    </Routes>
+  );
+};
+
+export default AppTestRouter;
