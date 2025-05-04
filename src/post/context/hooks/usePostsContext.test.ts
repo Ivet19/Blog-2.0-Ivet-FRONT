6bdd9f70143c9716b1110b3838434e1b97fcfe97
Ivet Lopez <ivet19@live.com>
2025-05-04 17:41:27 +0200

    Create usePostsContext error test

---
+++ src/post/context/hooks/usePostsContext.test.ts
@@ -0,0 +1,18 @@
+import { renderHook } from "@testing-library/react";
+import usePostsContext from "./usePostsContext";
+
+describe("Given the usePostsContext hook", () => {
+  describe("When it's called without context", () => {
+    test("Then it should throw 'Missing context for Post provider' error ", async () => {
+      const missingContextError = new Error(
+        "Missing context for Post provider",
+      );
+
+      const context = () => {
+        renderHook(() => usePostsContext());
+      };
+
+      expect(context).toThrow(missingContextError);
+    });
+  });
+});
